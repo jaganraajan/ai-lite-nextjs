a76ce6289e7acfcab42bb997559d1ff570827dd1
Jagan Raajan <jaganraajan@Jagans-MacBook-Air.local>
2025-04-12 14:02:08 -0400

    initial commit

---
+++ src/components/chat.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+const Chat: React.FC = () => {
+    return (
+        <div>
+            <h1>Chat Component</h1>
+            <p>Start your conversation here!</p>
+        </div>
+    );
+};
+
+export default Chat;

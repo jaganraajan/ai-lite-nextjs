a57c6fa2a93b6aa9d34df4a1b535ad62eba3284e
Jagan Raajan <jaganraajan@Jagans-MacBook-Air.local>
2025-04-15 19:36:23 -0400

    add aboutcard

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,24 +1,49 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from "@/components/ui/card";
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { IconArrowUp } from './ui/icons';
+import AboutCard from './cards/aboutcard';
 
-const Chat: React.FC = () => {
+
+export default function Chat() {
+    const [input, setInput] = useState<string>('');  
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        // const newMessages: CoreMessage[] = [
+        //   ...messages,
+        //   { content: input, role: 'user' },
+        // ];
+        // setMessages(newMessages);
+        setInput('');
+        // const result = await continueTextConversation(newMessages);
+        // for await (const content of readStreamableValue(result)) {
+        //   setMessages([
+        //     ...newMessages,
+        //     {
+        //       role: 'assistant',
+        //       content: content as string, 
+        //     },
+        //   ]);
+        // }
+      }
+
     return (
         <div className="group w-full overflow-auto ">
+            <AboutCard />  
             <div className="fixed inset-x-0 bottom-10 w-full ">
                 <div className="w-full max-w-xl mx-auto">
                 <Card className="p-2">
-                    <form onSubmit={() => console.log('test submit')}>
+                    <form onSubmit={handleSubmit}>
                     <div className="flex">
                         <Input
                         type="text"
-                        value={"test"}
+                        value={input}
                         onChange={event => {
-                            console.log(event.target.value);
+                            setInput(event.target.value);
                         }}
                         className="w-[95%] mr-2 border-0 ring-offset-0 focus-visible:ring-0 focus-visible:outline-none focus:outline-none focus:ring-0 ring-0 focus-visible:border-none border-transparent focus:border-transparent focus-visible:ring-none"
                         placeholder='Ask me anything...'
@@ -33,6 +58,4 @@ const Chat: React.FC = () => {
             </div>
         </div>
     );
-};
-
-export default Chat;
+}

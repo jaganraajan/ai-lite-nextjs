570921b3f335c744454254e7cd5ff1054b9f7d36
Jagan Raajan <jaganraajan@Jagans-MacBook-Air.local>
2025-04-22 18:40:58 -0400

    edit header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ import EnvCard from './cards/envcard'
 
 export async function Header() {
   return (
-    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-white ">  
+    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0">  
       <EnvCard />
       <Link href="/" rel="nofollow" className="mr-2 font-bold">
         Next.js AI Lite

90e67289a0d68bc4fadc449f09ff04307641ab3a
Jagan Raajan <jaganraajan@Jagans-MacBook-Air.local>
2025-04-22 18:49:31 -0400

    add social links in header section

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,33 +1,40 @@
 import * as React from 'react'
 import Link from 'next/link'
-import { cn } from '@/lib/utils'
-import { buttonVariants } from '@/components/ui/button'
-import { IconSeparator, IconVercel } from '@/components/ui/icons'
 import EnvCard from './cards/envcard'
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export async function Header() {
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0">  
       <EnvCard />
       <Link href="/" rel="nofollow" className="mr-2 font-bold">
-        Next.js AI Lite
+        Next.js Gemini AI
       </Link>
-      <IconSeparator />
-      <Link
-        href="/genui"
-        className={cn(buttonVariants({ variant: 'link' }), "mr-auto font-normal")}
-      >
-        <span className="hidden md:flex">GenUI</span>
-      </Link>
-      <Link
-        href="https://vercel.com/new"
-        target="_blank"
-        className={cn(buttonVariants())}
-      >
-        <IconVercel className="mr-2" />
-        <span className="hidden sm:block">Deploy to Vercel</span>
-        <span className="sm:hidden">Deploy</span>
-      </Link>
+      {/* Social Links */}
+      <ul className="flex space-x-6 items-center">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/jagan-raajan/"
+              className="flex items-center text-gray-300 hover:text-primary-400 transition-colors"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin size={24} />
+              <span className="ml-2 hidden sm:inline">jagan-raajan</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/jaganraajan/"
+              className="flex items-center text-gray-300 hover:text-primary-400 transition-colors"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub size={24} />
+              <span className="ml-2 hidden sm:inline">jaganraajan</span>
+            </a>
+          </li>
+        </ul>
     </header>
   )
 }
